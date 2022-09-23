025fec17763eafeacc9327b12382fc8533595602
hansdav <hdavidson21@gmail.com>
2022-09-23 20:21:34 +0200

    shipping out portofolio site. everything functioning

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 const slideButtons = document.querySelectorAll("[data-carousel-button]")
 
+const textContainer = document.querySelector(".text-container")
+const aboutProgramming = document.querySelector(".programming-text")
+const aboutLanguages = document.querySelector(".languages-text")
+const aboutCareer = document.querySelector(".career-text")
+const aboutGaming = document.querySelector(".gaming-text")
+
+const progButton = document.querySelector("#programming-btn")
+const languagesButton = document.querySelector("#languages-btn")
+const careerButton = document.querySelector("#career-btn")
+const gamingButton = document.querySelector("#gaming-btn")
+
+//functions for buttons re: about me //
+
+progButton.addEventListener("click", function() {
+    clearText();
+    textContainer.appendChild(aboutProgramming)
+});
+
+languagesButton.addEventListener("click", function() {
+    clearText();
+    textContainer.appendChild(aboutLanguages)
+});
+
+gamingButton.addEventListener("click", function() {
+    clearText();
+    textContainer.appendChild(aboutGaming)
+});
+
+//function that clears the about me text//
+
+function clearText() {
+    while (textContainer.firstChild) {
+        textContainer.replaceChildren();
+    }
+}
+
+//project slider carousel function//
+
 slideButtons.forEach(button => {
     button.addEventListener("click", () => {
         const offset = button.dataset.carouselButton === "next" ? 1 : -1
